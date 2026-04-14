04ef20ca1c6ec80acf6c3f621fcb423d5e71d757
Parashuram <parashurampawar56@gmail.com>
2026-04-14 19:56:25 +0530

    add program: [parashuram] added python script for preprocess review task program

---
+++ src/nlp_foundations/config/nltk_setup.py
@@ -0,0 +1,15 @@
+import nltk
+
+def download_nltk_resources():
+    resources = [
+        'punkt',
+        'punkt_tab',
+        'stopwords',
+        'wordnet',
+    ]
+
+    for resource in resources:
+        try:
+            nltk.data.find(resource)
+        except LookupError:
+            nltk.download(resource)
---
+++ src/nlp_foundations/src/preprocess_review.py
@@ -0,0 +1,38 @@
+import re
+import string
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
+from config.nltk_setup import download_nltk_resources
+
+download_nltk_resources()
+
+stop_words = set(stopwords.words('english'))
+lemmatizer = WordNetLemmatizer()
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'\d+', '', text)
+    text = text.translate(str.maketrans('', '', string.punctuation))
+    return text
+
+reviews = [
+    "Ranveer Singh delivers a career-best performance.",
+    "The film is intense, gripping, and keeps you hooked with its raw storytelling and strong emotional scenes.",
+    "Good concept, but the pacing feels slow at times.",
+    "Action-packed and engaging, though slightly long.",
+    "Strong visuals, but the story could be tighter."
+]
+
+
+cleaned = [clean_text(review) for review in reviews]
+tokens = [word_tokenize(text) for text in cleaned]
+filtered = [[word for word in review if word not in stop_words] for review in tokens]
+lemmatized = [[lemmatizer.lemmatize(w) for w in review] for review in filtered]
+
+print("\n")
+print(f"\nOriginal:\n{reviews}")
+print(f"\nCleaned:\n{cleaned}")
+print(f"\nTokens:\n{tokens}")
+print(f"\nFiltered:\n{filtered}")
+print(f"\nLemmatized:\n{lemmatized}")
